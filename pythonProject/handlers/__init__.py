2d1aab121dfc321877653ae0c2e2a85913b8d98e
lieyesis <yyilon98@gmail.com>
2024-12-29 05:21:11 +0600

    Delete pythonProject directory

--- pythonProject/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .start import start_router
-from .menu import menu_router
-from .random_meal import random_meal_router
-from .echo import echo_router
-from .review_dialog import review_router
-from .admin import admin_router
