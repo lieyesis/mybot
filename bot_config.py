1eaf81f81a9523f4b1aca1a27b89a287f848cfbb
lieyesis <yyilon98@gmail.com>
2024-12-29 15:34:04 +0600

    Delete bot_config.py

--- bot_config.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from dotenv import dotenv_values
-
-token = dotenv_values(".env")["BOT_TOKEN"]
-bot = Bot(token=token)
-dp = Dispatcher()
